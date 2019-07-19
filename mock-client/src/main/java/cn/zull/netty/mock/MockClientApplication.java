2dd486899e84e6e11713a35b84a573413a832200
ZuRun's MacBook Pro <hi@zull.cn>
2019-07-19 17:27:49 +0800

    项目搭建

---
+++ mock-client/src/main/java/cn/zull/netty/mock/MockClientApplication.java
@@ -0,0 +1,15 @@
+package cn.zull.netty.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zurun
+ * @date 2019/7/19 17:26:03
+ */
+@SpringBootApplication
+public class MockClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MockClientApplication.class, args);
+    }
+}
---
+++ mock-common/src/main/java/cn/zull/netty/mock/common/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zurun
+ * @date 2019/7/19 17:26:39
+ */
+package cn.zull.netty.mock.common;
