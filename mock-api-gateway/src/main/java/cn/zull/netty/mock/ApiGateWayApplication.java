a6b1b24e7f54d186df9ae809ea16d32f104bd777
ZuRun's MacBook Pro <hi@zull.cn>
2019-07-29 16:18:39 +0800

    增加api-gateway模块

---
+++ mock-api-gateway/src/main/java/cn/zull/netty/mock/ApiGateWayApplication.java
@@ -0,0 +1,15 @@
+package cn.zull.netty.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zurun
+ * @date 2019/7/29 16:12:13
+ */
+@SpringBootApplication
+public class ApiGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+}
